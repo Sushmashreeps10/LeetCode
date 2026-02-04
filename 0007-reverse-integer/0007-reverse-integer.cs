adc747f27b2abd4d7ea3a0c8f9c38ce9b75de00a
Sushmashreeps10 <sushre17@gmail.com>
2026-02-04 09:26:21 +0530

    Time: 29 ms (32.31%), Space: 28.6 MB (97.44%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int Reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+
+            if (rev > int.MaxValue / 10 || 
+               (rev == int.MaxValue / 10 && digit > 7)) {
+                return 0;
+            }
+
+            if (rev < int.MinValue / 10 || 
+               (rev == int.MinValue / 10 && digit < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+}
