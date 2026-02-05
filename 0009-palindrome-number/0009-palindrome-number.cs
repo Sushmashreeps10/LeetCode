d7ae9f1dda41eb286ce6676afc0eb33c692bf7c1
Sushmashreeps10 <sushre17@gmail.com>
2026-02-05 09:26:12 +0530

    Time: 1 ms (99.63%), Space: 34.6 MB (85.82%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        // Negative numbers or numbers ending with 0 (except 0)
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversedHalf = 0;
+
+        // Reverse only half of the number
+        while (x > reversedHalf)
+        {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even length: x == reversedHalf
+        // For odd length: x == reversedHalf / 10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+}
